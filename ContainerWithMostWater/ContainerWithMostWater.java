81b076da96a2bfaaea4941ed3b642c4216e0f7f9
RodEfraim <refraim@ucsd.edu>
2022-11-20 13:50:29 -0800

    Added comments to the ContainerWithMostWater class functions.

--- ContainerWithMostWater/ContainerWithMostWater.java
+++ ContainerWithMostWater/ContainerWithMostWater.java
@@ -2,6 +2,14 @@ package ContainerWithMostWater;
 
 import static java.lang.Math.abs;
 
+/**
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints
+ * of the ith line are (i, 0) and (i, height[i]).
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ * Return the maximum amount of water a container can store.
+ *
+ * Notice that you may not slant the container.
+ */
 public class ContainerWithMostWater {
 
     public static int maxArea(int[] height) {
@@ -38,6 +46,15 @@ public class ContainerWithMostWater {
             return false;
     }
 
+    /**
+     * Have two pointers. lPointer starts at index 0. rPointer starts at n-1.
+     * The value of the pointer that has the lowest value will be the one to move.
+     *
+     * @param height The array of ints.
+     * @param lPointer The pointer that starts left of the array.
+     * @param rPointer The pointer that starts right of the array.
+     * @return maxArea: maximum amount of water a container can store
+     */
     public static int comparePointers(int[] height, int lPointer, int rPointer){
 
         int area = 0;
